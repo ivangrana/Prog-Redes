073b60d281c7fa60151013d7f466d5250cc3d633
Ivan Grana <89265428+ivangrana@users.noreply.github.com>
2023-02-02 20:39:12 -0300

    .

--- TCP/servidor_TCP.py
+++ TCP/servidor_TCP.py
@@ -1,7 +1,6 @@
 #Criação de um servidor utilizando o protocolo TCP
-
 import socket
-import threading
+
 SERVER_IP = "127.0.0.1"
 SERVER_PORT = 9998
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -10,14 +9,18 @@ server.listen(5)
 print("[*] Server em modo escuta %s:%d" % (SERVER_IP,SERVER_PORT))
 client,addr = server.accept()
 client.send("Aceitando conexão...".encode())
-print("[*] Conexão aceita com: %s:%d" %(addr[0],addr[1]))
+print("[+] Conexão aceita com: %s:%d" %(addr[0],addr[1]))
 
-def handle_client(client_socket):
+def request_cliente(client_socket:socket.socket):
+    """Função responsável por receber a mensagem do cliente no servidor"""
     request = client_socket.recv(1024)
-    print("[*] Pedido aceito: %s do cliente %s" , request, client_socket.getpeername())
-    client_socket.send(bytes("ACK","utf-8"))
-    while True:
-        handle_client(client)   
+    request = bytes(request)
+    print("[+] Mensagem recebida: %s do usuário %s" %(request.decode('UTF-8'), client_socket.getpeername()))
+    client_socket.send(bytes("ACK ","utf-8"))
+    client_socket.send(bytes("Mensagem envida com sucesso !\n","utf-8"))
+    
+
+while True:
+    request_cliente(client)   
         
-    client_socket.close()
-    server.close()
+    
